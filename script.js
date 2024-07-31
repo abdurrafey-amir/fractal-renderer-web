880856a1fbc10d88fd87814fa69be725b174c9f3
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-07-31 16:18:50 +0500

    added the julia set, added pan, zoom on click, finished

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var header = document.querySelector('h2')
 var canvas = document.querySelector('canvas')
 var ctx = canvas.getContext('2d')
 
-var width = 200
-var height = 200
+var width = 400
+var height = 400
 
 canvas.width = width
 canvas.height = height
@@ -12,13 +12,32 @@ var mouseX = 0
 var mouseY = 0
 
 var constant = math.complex(0.28, 0.01)
+var maxIterations = 64
+var clicked = false
+var pan = math.complex(0, 0)
+var zoom = 1
+
+function julia(z, i = 0) {
+    // julia set formula
+    z = z.mul(z)
+    z = z.add(constant)
+
+    if (math.abs(z) > 2 || i == maxIterations) {
+        return i
+    } else {
+        return julia(z, i + 1)
+    }
+}
 
 // turn a point on the complex plane into a color
 function pointToColor(point) {
-    point = point.sub(constant)
-    var red = point.re * 255
-    var green = point.im * 255
-    var blue = math.abs(point) * 255
+    var iterations = julia(point)
+
+    var percentage = iterations / maxIterations
+
+    var red = percentage * 255
+    var green = percentage * 255
+    var blue = percentage * 255
 
     return `rgb(${red}, ${green}, ${blue})`
 }
@@ -31,7 +50,15 @@ function pixelToPoint(x, y) {
     var zy = 1 - (y / height) * 2
 
     // create a complex number based on our new xy values
-    return math.complex(zx, zy)
+    var z =  math.complex(zx, zy)
+
+    // zoom
+    z = z.div(zoom)
+
+    // pan
+    z = z.add(pan)
+
+    return z
 }
 
 function drawPixel(x, y, color) {
@@ -53,11 +80,31 @@ function draw() {
 }
 
 function update() {
-    header.innerHTML = constant.toString()
+    header.innerHTML = constant.toString() + ' at ' + zoom + 'X'
     draw()
 }
 
+function click(event) {
+    if (!clicked) {
+        clicked = true
+        return
+    }
+
+    mouseX = event.clientX - canvas.offsetLeft
+    mouseY = event.clientY - canvas.offsetTop
+
+    pan = pixelToPoint(mouseX, mouseY)
+
+    zoom *= 2
+
+    update()
+}
+
 function move(event) {
+    if (clicked) {
+        return
+    }
+    
     mouseX = event.clientX - canvas.offsetLeft;
     mouseY = event.clientY - canvas.offsetTop;
 
@@ -68,6 +115,8 @@ function move(event) {
 
     update()
 }
+
+canvas.addEventListener('click', click)
 
 canvas.addEventListener('pointermove', move)
 update()
